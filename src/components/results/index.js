3a28564ad2a77c227b609d15b764d7a07646c7cf
Gus Rodriguez <grodriguez@medallia.com>
2020-07-08 21:12:15 -0300

    draft carousels working

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import Slide from 'react-reveal/Slide';
+import makeCarousel from 'react-reveal/makeCarousel';
 import Header from '../header';
 import Card from '../card';
-import { StyledResultsContainer } from './styled';
+import { StyledResultsContainer, StyledArrow, StyledPage } from './styled';
 
 function Results() {
+  const page = (
+    <StyledPage>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </StyledPage>
+  );
+
+  const CarouselUI = ({ position, handleClick, children }) => <StyledResultsContainer>
+    {children}
+    <StyledArrow onClick={handleClick} data-position={position - 1}>{'<'}</StyledArrow>
+      <StyledArrow right onClick={handleClick} data-position={position + 1}>{'>'}</StyledArrow>
+    </StyledResultsContainer>;
+  const Carousel = makeCarousel(CarouselUI);
+
   return (
+
     <React.Fragment>
       <Header />
-      <StyledResultsContainer>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </StyledResultsContainer>
+        <Carousel defaultWait={0}>
+          <Slide right>
+            {page}
+          </Slide>
+          <Slide right>
+            {page}
+          </Slide>
+          <Slide right>
+            {page}
+          </Slide>
+        </Carousel>
     </React.Fragment>
   );
 }
--- src/components/results/styled.js
+++ src/components/results/styled.js
@@ -1,25 +1,33 @@
 import styled from 'styled-components';
+import  { css } from 'styled-components';
 
 export const StyledResultsContainer = styled.div`
-  padding-top: 43px;
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
-  width: 95%;
+  border: 1px solid red;
   position: relative;
   overflow: hidden;
+  width: 100%;
+  height: 400px;
+  justify-content: center;
+  margin: 0 auto;
 `;
 
-export const Arrow = styled.div`
+export const StyledArrow = styled.div`
   text-shadow: 1px 1px 1px #fff;
+  color: white;
   z-index: 100;
   line-height: 1px;
   text-align: center;
   position: absolute;
-  top: 0;
+  top: 200px;
   width: 10%;
   font-size: 3em;
   cursor: pointer;
   user-select: none;
   ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
 `;
+
+export const StyledPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
