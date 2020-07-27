4545df56a3fcae2b5731ff8f24bcbace5919e836
Gus Rodriguez <grodriguez@medallia.com>
2020-07-27 13:10:03 -0300

    add hover effect to the autosuggest

--- src/components/search/styled.ts
+++ src/components/search/styled.ts
@@ -17,14 +17,24 @@ export const StyledForm = styled.form`
     background: ${colors.white};
     position: relative;
   }
+  & .react-autosuggest__suggestions-list {
+    margin: 0;
+    padding: 0;
+  }
   & li {
     list-style: none;
-    padding-bottom: ${sizes.xs};
+    padding: 12px 12px 12px 24px;
+    &:hover {
+      background: ${colors.darkred}
+    }
+    transition: 0.2s;
+    cursor: pointer;
   }
 `;
 
 export const IconCloseWrapper = styled.div`
   margin: ${sizes.xs};
+  cursor: pointer;
   position: absolute;
   right: 0;
 `;
