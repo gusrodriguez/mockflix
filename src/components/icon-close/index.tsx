ee5083602157c0eb9496d64a08c9e589bc017d07
Gus Rodriguez <grodriguez@medallia.com>
2020-08-02 20:57:52 -0300

    remove unnecessary classname in svg component

--- src/components/icon-close/index.tsx
+++ src/components/icon-close/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function IconClose() {
   return (
-    <svg className="icon-close" width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+    <svg width="18px" height="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
       <polygon
         fill="#000000"
         points="96,14 82,0 48,34 14,0 0,14 34,48 0,82 14,96 48,62 82,96 96,82 62,48"
